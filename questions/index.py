a318f2bd613610dadaab664b0beceae839e88ca3
Nguyen Anh <nngguyen.anh@gmail.com>
2023-05-18 20:29:35 +0700

    Na push code 1

--- questions/index.py
+++ questions/index.py
@@ -1,54 +1,31 @@
-from questions.House_Type import House_Type_Question
 from questions.Price_Correlation import Price_Correlation_Question
 from questions.House_Price_Fluctuation import Price_Fluctuation_Question
 from questions.District_Information import District_Information_Question
-from questions.house_rooms_correlation import houseRoomsCorrelation
-from questions.More_House_Less_Price import More_House_Less_Price_Question
-from questions.Regression import Regression_Question
-from questions.Price_followed_by_District import Price_followed_by_district_Question
+from questions.Regression import Regression
 from utils.index import get_hash
 
 def get_questions():
     questions = [
         {
-            "component": House_Type_Question,
-            "name": "🏚 Q1 House Type",
-            "icon": "front"
-        },
-        {
             "component": Price_Correlation_Question,
-            "name": "💰 Q2 Price correlation",
+            "name": "💰 Q1 Price correlation",
             "icon": "cash-coin"
         },
         {
             "component": Price_Fluctuation_Question,
-            "name": "📉 Q3 Price Fluctuation",
+            "name": "📉 Q2 Price Fluctuation",
             "icon": "line-chart"
         },
         {
             "component": District_Information_Question,
-            "name": "🌆 Q4 District Information",
+            "name": "🌆 Q3 District Information",
             "icon": "home-night"
         },
-        {
-            "component": houseRoomsCorrelation,
-            "name": "🤖 Q5 House Rooms Correlation",
-            "icon": "cash-coin"
-        },
-        {
-            "component": More_House_Less_Price_Question,
-            "name": "🏚 Q6 More House Less Price",
-            "icon": "front"
-        },
         {    
-            "component": Regression_Question,
-            "name": "📈 Q7 Regression",
+            "component": Regression,
+            "name": "📈 Q4 Regression",
             "icon": "line-chart"
         },
-        {
-            "component": Price_followed_by_district_Question,
-            "name": "📈 Q8 Price followed by district",
-            "icon": "line-chart"
-        }]
+    ]
 
     return get_hash(questions)
--- screens/index.py
+++ screens/index.py
@@ -1,6 +1,6 @@
 from screens.dataCollection import Collection_Screen
 from screens.dataExploration import Exploration_Screen
-# from screens.analysis import Analysis_Screen
+from screens.dataAnalysis import Analysis_Screen
 # from screens.reflection import Reflection_Screen
 from utils.index import get_hash
 
@@ -16,11 +16,11 @@ def get_routes():
             "name": "Exploration",
             "icon": "folder2"
         },
-        # {
-        #     "component": Analysis_Screen,
-        #     "name": "Analysis",
-        #     "icon": "bar-chart-line"
-        # },
+        {
+            "component": Analysis_Screen,
+            "name": "Analysis",
+            "icon": "bar-chart-line"
+        },
         # {
         #     "component": Reflection_Screen,
         #     "name": "Reflection",
